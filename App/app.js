478be15723a4afe983e837d09ac889f234a4f218
Krishna Kant Sharma <krishnakantsharma560@gmail.com>
2025-04-02 12:53:24 +0530

    cors setup

--- App/app.js
+++ App/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors({
   credentials:true,
-  origin:["https://sparkly-clafoutis-171ffd.netlify.app", "http://localhost:5173"],
+  origin:["https://radiant-concha-6f5f30.netlify.app", "http://localhost:5173"],
 }))
 
 
